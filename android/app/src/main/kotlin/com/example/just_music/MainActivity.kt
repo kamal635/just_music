9bac226ec2b489c53c54124d1820231fa253e7f5
kamal635 <kokoangry51@gmail.com>
2024-05-24 11:41:02 -0400

    create new project now

---
+++ android/app/src/main/kotlin/com/example/just_music/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.just_music
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const JustMusicApp());
+}
+
+class JustMusicApp extends StatelessWidget {
+  const JustMusicApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
