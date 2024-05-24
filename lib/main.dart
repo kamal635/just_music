8a4b2ae87fc6902321749a093e9d000e40799954
kamal635 <kokoangry51@gmail.com>
2024-05-24 12:05:47 -0400

    started new branch

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,6 @@ class JustMusicApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: TestGit(),
-    );
-  }
-}
-
-class TestGit extends StatelessWidget {
-  const TestGit({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp();
   }
 }
