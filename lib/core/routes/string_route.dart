4678cfb936c9bb90dd78f47830953e48fd607817
kamal635 <kokoangry51@gmai.com>
2024-08-16 03:21:13 -0700

    create new branch settings_view

--- lib/core/routes/string_route.dart
+++ lib/core/routes/string_route.dart
@@ -1,11 +1,12 @@
 abstract class RouterName {
   static const changedView = "/";
   static const homeView = "/homeView";
+  static const songsView = "/songsView";
   static const songsArtist = "/songsArtist";
   static const songsAlbums = "/songsAlbums";
   static const recentlyPlayedView = "/recentlyPlayedView";
+  static const settingsView = "/settingsView";
   static const mostPlayedView = "/mostPlayedView";
-  static const songsView = "/songsView";
   static const playListView = "/PlayListView";
   static const favoriteView = "/FavoriteView";
   static const listOfSongsView = "/listOfSongsView";
